e8d398afb596031721bae36d449b37eb94b8bb7a
nano.son <nano.son@kakaocorp.com>
2019-07-14 23:04:45 +0900

    embeded tomcat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
+import org.apache.catalina.startup.Tomcat
+import java.io.File
+
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("hello world")
+            val tomcat = Tomcat().apply {
+                setPort(8080)
+                addWebapp("/", File("src/main/myapp").absolutePath)
+                connector
+            }
+            tomcat.start()
+            tomcat.server.await()
         }
     }
 }
