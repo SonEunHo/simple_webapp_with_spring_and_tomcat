a9db412a122487e4058e0ac17ad1e43ea08fa899
nano.son <nano.son@kakaocorp.com>
2019-07-14 23:18:24 +0900

    spring mvc

---
+++ src/main/kotlin/com/nanostudio/webapp/config/MyAppInitializer.kt
@@ -0,0 +1,20 @@
+package com.nanostudio.webapp.config
+
+import org.springframework.web.WebApplicationInitializer
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
+import javax.servlet.ServletContext
+
+class MyAppInitializer: WebApplicationInitializer {
+    override fun onStartup(servletContext: ServletContext) {
+        val myApplicationContext = AnnotationConfigWebApplicationContext()
+            .apply { register(MyConfiguration::class.java) }
+
+        val myDispatcherServlet = DispatcherServlet(myApplicationContext)
+
+        servletContext.addServlet("myDispatcherServlet", myDispatcherServlet)
+            .also {
+                it.addMapping("/")
+            }
+    }
+}
